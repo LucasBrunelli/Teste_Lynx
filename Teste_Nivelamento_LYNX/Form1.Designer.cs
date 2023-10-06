41dd718d380dcd1adff0febc518964ec7189b485
LucasBrunelli <brunelli_lucas@hotmail.com>
2023-10-06 11:06:22 -0300

    Primeiro commit

---
+++ Teste_Nivelamento_LYNX/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Teste_Nivelamento_LYNX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Executar Desafio 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 228);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Entrada N";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(44, 334);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Saida Peças";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Executar Desafio 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ultimo denominador";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(315, 228);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(167, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(377, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Saida S";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(315, 334);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(167, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(609, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 38);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Executar Desafio 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(637, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Hora da saida";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(589, 186);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(167, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(625, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Tempo de viagem";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(589, 228);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(167, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(627, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Diferença de fuso";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(589, 271);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(167, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(629, 318);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Hora de chegada";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(589, 334);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(167, 20);
+            this.textBox8.TabIndex = 17;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(44, 83);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(167, 72);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "Numero maximo de peças em um jogo de dominó com a maior peça sendo o numero duplo" +
+    " N";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(315, 86);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(167, 69);
+            this.richTextBox2.TabIndex = 20;
+            this.richTextBox2.Text = "Somatorio de uma serie de fraçoes, começando de 1 + 1/2 + 1/3 ... até 1/Ultimo De" +
+    "nominador escolhido na caixa abaixo.";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(589, 86);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(167, 69);
+            this.richTextBox3.TabIndex = 21;
+            this.richTextBox3.Text = "Programação de viagem, com base na hora de saida, tempo de viagem e diferença de " +
+    "fuso horario";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ Teste_Nivelamento_LYNX/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste_Nivelamento_LYNX
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // Trantando o evento de click do Desafio 1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            // Identificando se foi digitado um valor inteiro
+
+            if (int.TryParse(textBox1.Text, out int Pecas_OUT))
+            {
+                // Executando a formula para descobrir o numero de peças
+
+                Pecas_OUT = ((Pecas_OUT + 1) * (Pecas_OUT + 2)) / 2;
+                textBox2.Text = Pecas_OUT.ToString();
+            }
+            else
+            {
+                // Solicitando ao usuario que insira um valor inteiro
+
+                MessageBox.Show("Por favor, insira um valor inteiro válido.");
+            }
+
+              
+        }
+
+
+
+        // Trantando o evento de click do Desafio 2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float Valor_S = 0,
+                  Aux_Den = 0;
+
+            // Identificando se foi digitado um valor inteiro
+
+            if (int.TryParse(textBox3.Text, out int Denominador))
+            {
+                // Denominador é o ultimo denominador de fração da serie escolhida
+                // para caso queremos um somatorio de frações maiores ou menores
+                // Assim executamos o calculo do somatorio ate a ultima fração ser 1
+             
+                while (Denominador > 0)
+                {
+                    Aux_Den = Denominador;
+                    Valor_S = Valor_S + (1 / Aux_Den);
+                    Denominador--;
+                }
+
+                textBox4.Text = Valor_S.ToString("N2");
+            }
+            else
+            {
+                // Solicitando ao usuario que insira um valor inteiro
+
+                MessageBox.Show("Por favor, insira um valor inteiro válido.");
+            }
+        }
+
+
+
+
+        // Trantando o evento de click do Desafio 3
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            int Hora_Chegada = 0;
+
+            // Identificando se foi digitado um valor inteiro
+
+            if ((int.TryParse(textBox5.Text, out int Hora_Saida))
+                && (int.TryParse(textBox6.Text, out int TIME_Viagem))
+                && (int.TryParse(textBox7.Text, out int Delta_Fuso)))
+            {
+
+                // Identificando se foi digitado um valor dentro dos limites
+
+                if ((Hora_Saida  <= 23) && (Hora_Saida  >=  0)
+                    && (TIME_Viagem <= 12) && (TIME_Viagem >=  1)
+                    && (Delta_Fuso  <=  5) && (Delta_Fuso  >= -5))
+                {
+
+                    // Calculando a hora de chegada
+
+                    Hora_Chegada = Hora_Saida + TIME_Viagem + Delta_Fuso;
+                    if(Hora_Chegada > 23)
+                    {
+                        // Caso a hora ulltrapasse para o proximo dia
+                        // Colocar no padrao de 24 horas
+
+                        Hora_Chegada = Hora_Chegada - 24;
+                    }
+
+                    textBox8.Text = Hora_Chegada.ToString();
+                }
+                else
+                {
+
+                    // Valores não estão dentro dos limites
+
+                    MessageBox.Show("Por favor, a hora de saída deve corresponder de 0 a 23,"
+                                   + " o tempo de viagem de 1 a 12,"
+                                   + " e o fuso horário de -5 a 5.");
+                }
+             
+            }
+            else
+            {
+                // Solicitando ao usuario que insira um valor inteiro
+
+                MessageBox.Show("Por favor, insira um valor inteiro válido.");
+            }
+
+        }
+
+
+
+    }
+}
